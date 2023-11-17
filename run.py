4a10447c1a4bc5b5099d08e11763c8ad25d39dd2
Allen Ting <allenkting1@gmail.com>
2023-11-17 02:15:16 -0600

    pip problems

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-import datasets
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, \
     AutoModelForQuestionAnswering, Trainer, TrainingArguments, HfArgumentParser
 from helpers import prepare_dataset_nli, prepare_train_dataset_qa, \
     prepare_validation_dataset_qa, QuestionAnsweringTrainer, compute_accuracy
 import os
 import json
+import datasets
 
 NUM_PREPROCESSING_WORKERS = 2
 
